246396a20146d330e0ed0f5609313376042a4cd1
Shyam Kaushik <kaushikshyam50@gmail.com>
2023-09-21 18:00:40 +0530

    Modified decimal to binary

--- DecimalTobinary.cpp
+++ DecimalTobinary.cpp
@@ -2,18 +2,18 @@
 #include<math.h>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    int i=0;
-    int ans=0;
-    while(n!=0){
-        int digit=n%10;
-        if(digit==1)
-            ans=ans+pow(2,i);
-        
-        n=n/10;
-        i++;
-
-    }
-    cout<<ans;
+    //code is correct but vs code is showing wrong output for 5 , showing 100 but actual ans is 101 
+    //all other compiler showing correct output 
+ int n;
+ cout<<"Enter the  decimal number to convert it in binary "<<endl;
+ cin>>n;
+ int ans=0;
+ int i=0;
+ while(n!=0){
+    int bit =n&1;
+    ans=(bit*pow(10,i))+ans;
+    n=n>>1;
+    i++;
+ }
+ cout<<ans;
 }
